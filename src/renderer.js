4374f79816ec79a3e15d786c2c1f2c513998f488
Kavindu Pramod <kavindu.20210410@iit.ac.lk>
2025-03-14 12:27:55 +0530

    initial commit

---
+++ src/renderer.js
@@ -0,0 +1,44 @@
+/**
+ * This file will automatically be loaded by vite and run in the "renderer" context.
+ * To learn more about the differences between the "main" and the "renderer" context in
+ * Electron, visit:
+ *
+ * https://electronjs.org/docs/tutorial/process-model
+ *
+ * By default, Node.js integration in this file is disabled. When enabling Node.js integration
+ * in a renderer process, please be aware of potential security implications. You can read
+ * more about security risks here:
+ *
+ * https://electronjs.org/docs/tutorial/security
+ *
+ * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
+ * flag:
+ *
+ * ```
+ *  // Create the browser window.
+ *  mainWindow = new BrowserWindow({
+ *    width: 800,
+ *    height: 600,
+ *    webPreferences: {
+ *      nodeIntegration: true
+ *    }
+ *  });
+ * ```
+ */
+
+import './index.css';
+import packageJson from '../package.json';
+
+console.log('👋 This message is being logged by "renderer.js", included via Vite');
+
+// function to display the version from package.jhson
+function displayVersion() {
+    
+    const version = packageJson.version;
+    const versionElement = document.getElementById('version');
+    versionElement.innerText = "v" + version;
+  }
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    displayVersion();
+  });
